ed89699b2cd7db3ea5002a3a4af3bd38c6b26d8f
CaptainCredible <cpt.credible@gmail.com>
2021-11-08 18:02:43 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    led.toggleAll()
+motherBrain.unMuteAllMusicians()
+})
+motherBrain.makeMotherBrain()
+motherBrain.changeName(2, "Rab")
+motherBrain.changeName(3, "Bun")
+motherBrain.accelMute()
